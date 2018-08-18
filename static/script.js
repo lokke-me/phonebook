5ed7efc7e2409f67e7b71bd0577e408d0ed830bd
Grischa Wende <admin@grischawen.de>
2018-08-18 18:35:30 +0200

    Fixed online status indicator

--- static/script.js
+++ static/script.js
@@ -9,10 +9,19 @@ var app = new Vue({
             online: false,
         }
     },
+    created: function(){
+        this.getData();
+    },
     methods: {
         loginWindow: function(){
-            popup = window.open('https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=<client_id>&redirect_url=http%3A%2F%2Flocalhost%3A5000%2Flogin&response_type=code&scope=Contacts.Read%20offline_access');
+            popup = window.open('https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=<client_id>&redirect_url=http%3A%2F%2Flocalhost%3A5000%2Flogin&response_type=code&scope=Contacts.Read%20offline_access', '_self');
             console.log(popup);
+        },
+        getData: function(){
+            axios.get('/status').then(function(res){
+                console.log(res);
+                app.status.online = res.data.status;
+            });
         }
     }
 });
